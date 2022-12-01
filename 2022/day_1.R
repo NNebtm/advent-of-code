8406b85f603ab7300744550f554f857b174e938f
EBTM (Esben Tore Mølgaard) <ebtm@novonordisk.com>
2022-12-01 14:48:49 +0100

    Added second solution using base::split.

--- 2022/day_1.R
+++ 2022/day_1.R
@@ -18,3 +18,23 @@ which(elf_vec == max(elf_vec))
 max(elf_vec)
 
 sort(elf_vec, decreasing = TRUE)[1:3] %>% sum
+
+
+#Solution using split
+
+j <- 1
+split_fac <- c()
+for (i in seq_along(pzl_data)) {
+  if(pzl_data[i] == "") {
+    split_fac[i] <- 0
+    j <- j + 1
+  } else {
+    split_fac[i] <- j
+  }
+}
+split_fac <- factor(split_fac)
+
+elf_list <- sapply(split(pzl_data, split_fac), function(x) sum(as.integer(x)))
+max(elf_list, na.rm = TRUE)
+
+sort(elf_list, decreasing = TRUE)[1:3] %>% sum(na.rm = TRUE)

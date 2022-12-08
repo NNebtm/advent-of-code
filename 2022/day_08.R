f3f8be9f1c88325ba58e4dd3bfe0895b654bbfe2
EBTM (Esben Tore Mølgaard) <ebtm@novonordisk.com>
2022-12-08 08:52:19 +0100

    Added comments.

--- 2022/day_08.R
+++ 2022/day_08.R
@@ -12,35 +12,41 @@ pzl_test <- c("30373",
 library(purrr)
 
 pzl_mat <- matrix(unlist(map(strsplit(pzl_data, ""), as.integer)), ncol = 99, byrow = TRUE)
-pzl_mat <- matrix(unlist(map(strsplit(pzl_test, ""), as.integer)), ncol = 5, byrow = TRUE)
+# pzl_mat <- matrix(unlist(map(strsplit(pzl_test, ""), as.integer)), ncol = 5, byrow = TRUE)
 
 m_max <- dim(pzl_mat)[1]
 n_max <- dim(pzl_mat)[2]
 
 vis_mat <- matrix(rep(0,m_max*n_max), ncol = m_max, byrow = TRUE)
 
+# Loop through all trees. If we are on an edge or if the height is greater than 
+# all trees in any of the four directions, it is visible.
 for (m in 1:m_max) {
   for (n in 1:n_max) {
     if (m %in% c(1,m_max) |
         n %in% c(1,n_max)) {
       vis_mat[m,n] <- 1
-      # print(paste(m,n,1))
     } else if (all(pzl_mat[m,n] > pzl_mat[1:(m-1),n]) |
                all(pzl_mat[m,n] > pzl_mat[(m+1):m_max,n]) |
                all(pzl_mat[m,n] > pzl_mat[m,1:(n-1)]) |
                all(pzl_mat[m,n] > pzl_mat[m, (n+1):n_max])) {
       vis_mat[m,n] <- 1
-      # print(paste(m,n,1))
     }
-    # print(paste(m,n))
   }
 }
 
+#Turn the visibility matrix into an integer vector and sum it.
 vis_mat %>% as.list %>% unlist %>% sum
 
 #Part 2
 sce_mat <- matrix(rep(0,m_max*n_max), ncol = m_max, byrow = TRUE)
 
+# Loop through all trees. If we are on an edge, the scenic score is 0.
+# If not, look each each cardinal direction and compare the tree's height to theirs.
+# cumprod gives us a vector with 1's until we hit the first tree that is taller,
+# and then 0's from there. Add up the vector and if it ended because we found a 
+# taller tree, add one.
+# Multiply the scenic scores for each direction.
 for (m in 1:m_max) {
   for (n in 1:n_max) {
     if (m %in% c(1,m_max) |
@@ -77,5 +83,6 @@ for (m in 1:m_max) {
   }
 }
 
+#Convert the matrix to an integer vector and find the max.
 sce_mat %>% as.list %>% unlist %>% max
 
